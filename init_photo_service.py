48623711288aecd93ab152fedefacc20b4defb3b
Gil Rosenberg <gil.rosenberg@mail.huji.ac.il>
2021-06-19 14:39:16 +0300

    finish init service

--- init_photo_service.py
+++ init_photo_service.py
@@ -2,10 +2,10 @@ import os
 from Google import Create_Service
 import pandas
 
-API_NAME = 'photosLibrary'
+
+API_NAME = 'photoslibrary'
 API_VERSION = 'v1'
 CLIENT_SECRET_FILE = 'client_secret_GooglePhotosVisualization.json'
 SCOPES = ['https://www.googleapis.com/auth/photoslibrary',
           'https://www.googleapis.com/auth/photoslibrary.sharing']
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-
